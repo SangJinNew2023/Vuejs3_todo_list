b8057f554cf8863bb53915ba3e70ccffa2422b5d
Sang jin An <photoan79@naver.com>
2023-03-24 00:32:43 -0700

    5chapter finished

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="name">
+    {{ greet }}
+  </div>
+
+</template>
+
+<script>
+export default {
+  setup() {
+    const name = 'Sangjin Coder!';
+
+    const greeting = (name) => {
+      return 'Hello!' + name;
+    };
+
+    const greet = greeting(name);
+
+    return {
+      greet
+    };
+  }
+}
+</script>
+
+<style>
+  .name {
+    color: red;
+  }
+</style>
