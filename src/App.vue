fd854e567fee9f1928349333280ae0474e27680b
Sang jin An <photoan79@naver.com>
2023-03-24 00:50:10 -0700

    7chapter finished

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="name">
-    {{ greet }}
+    {{ name }}
   </div>
-
+  <button class="btn btn-primary" v-on:click="updateName"> Click </button>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   setup() {
-    const name = 'Sangjin Coder!';
+    const name = ref('Sangjin Coder!'); //ref로 감싸서 변경 가능
 
-    const greeting = (name) => {
-      return 'Hello!' + name;
+    // const greeting = (name) => {
+    //   return 'Hello!' + name;
+    // };
+
+    // const greet = greeting(name);
+
+    const updateName = () => {
+      name.value = 'Cossie Coder1'; //ref로 감싼것은 .value를 써서 호출
+      console.log(name)
     };
 
-    const greet = greeting(name);
-
     return {
-      greet
+      name,
+      updateName,
     };
   }
 }
